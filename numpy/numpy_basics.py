536419d306f2913f924e5a42fef715470dd553a6
Asit Sengar <asitsengar28@gmail.com>
2026-06-02 22:57:24 +0530

    made some changes

--- numpy/numpy_basics.py
+++ numpy/numpy_basics.py
@@ -6,3 +6,7 @@ c = a + b
 print(a)
 print(b)
 print(c)
+
+# we can create an array from a number to a number with equal spacing by using linspace function
+d = np.linspace(10, 20, 5, dtype="int8")
+print("the array is : ", d)
