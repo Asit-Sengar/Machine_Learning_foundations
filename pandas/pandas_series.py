1b582b296551ecf3e0eb766a7758d57f8b969b52
Asit Sengar <asitsengar28@gmail.com>
2026-06-03 12:24:16 +0530

    added is_unique method

--- pandas/pandas_series.py
+++ pandas/pandas_series.py
@@ -14,3 +14,14 @@ subjects = ["maths", "english", "science"]
 b = pd.Series(marks, index=subjects, name="my marks", dtype="int8")
 # and the marks array as answer
 print(b)
+
+# we can create pandas series from dict also
+marks_dict = {
+    "maths": 12,
+    "english": 34,
+    "science": 56
+}
+marks_series = pd.Series(marks_dict, dtype="int16")
+print(marks_series)
+# .isunique function tells if the series have all unique elements
+print(pd.Series([1, 2, 3, 4, 5, 5]).is_unique)
