9e7edb437bdddf72332ab56903c1faa0b94eb28f
Asit Sengar <asitsengar28@gmail.com>
2026-06-06 15:22:07 +0530

    made some changes in pandas series

--- pandas/pandas_dataframe.py
+++ pandas/pandas_dataframe.py
@@ -1,8 +1,20 @@
 # it will contain all important dataframes methods
 import pandas as pd
 print("the python version is: ", pd.__version__)
-arr = [1, 2, 3, 4, 5]
-indeces = ["maths", "phy", "chem", "hindi", "sans"]
-series = pd.Series(arr, index=indeces)
-print(series)
-print(series.loc["chem"])
+arr = [1, 2, 3, 4, 5, 444, 555]
+indeces = ["maths", "phy", "chem", "hindi", "sans", "geo", "english"]
+series = pd.Series(arr, index=indeces, dtype="int16")
+print("the series object is: ", series)
+print("the value at chem location is: ", series.loc["chem"])
+series.loc["sst"] = 6  # here i am assigning the value of sst location
+print("the value of sst is: ", series["sst"])
+print(series[series > 100])
+
+
+# now we will use dictionary
+
+calories = {"day1": 1749, "day2": 3344, "day3": 5667}
+series_dict = pd.Series(calories, dtype="int16")
+print(series_dict)
+print("the calories for day3 are: ", series_dict.loc["day3"])
+print("days greater than 2000 calories: ", series_dict[series_dict > 2000])
--- pandas/pandas_series.py
+++ pandas/pandas_series.py
@@ -27,11 +27,22 @@ print(marks_series)
 print(pd.Series([1, 2, 3, 4, 5, 5]).is_unique)
 
 
-df = pd.read_csv("../datasets/subs.csv")
-print(df)
-print(type(df))  # we can see the type of data also
+# it will contain all important dataframes methods
+print("the python version is: ", pd.__version__)
+arr = [1, 2, 3, 4, 5, 444, 555]
+indeces = ["maths", "phy", "chem", "hindi", "sans", "geo", "english"]
+series = pd.Series(arr, index=indeces, dtype="int16")
+print("the series object is: ", series)
+print("the value at chem location is: ", series.loc["chem"])
+series.loc["sst"] = 6  # here i am assigning the value of sst location
+print("the value of sst is: ", series["sst"])
+print(series[series > 100])
 
-kohli_df = pd.read_csv("C:\rough_folder\Machine_Learning_foundations\datasets\kohli_ipl.csv",
-                       index="match_no", squeeze=True)
-print(kohli_df)
-print(type(kohli_df))
+
+# now we will use dictionary
+
+calories = {"day1": 1749, "day2": 3344, "day3": 5667}
+series_dict = pd.Series(calories, dtype="int16")
+print(series_dict)
+print("the calories for day3 are: ", series_dict.loc["day3"])
+print("days greater than 2000 calories: ", series_dict[series_dict > 2000])
