f101fc8d49707a9cc2d3e4e520f7d5c143c57f5b
Asit Sengar <asitsengar28@gmail.com>
2026-06-07 00:51:38 +0530

    modified pandas dataframe

--- pandas/pandas_dataframe.py
+++ pandas/pandas_dataframe.py
@@ -5,4 +5,6 @@ data = {"names": ["asit", "sakshi", "khushi"],
 df = pd.DataFrame(data, index=["employe1", "employe2", "employe3"])
 print("the data frame is: ", df)
 # to print something at specific location
-print("at employe2 location is: ", df.loc["employe2"])
+print(df.loc["employe2"])
+df["job"] = ["software engineer", "housewife", "housewife"]
+print(df)
