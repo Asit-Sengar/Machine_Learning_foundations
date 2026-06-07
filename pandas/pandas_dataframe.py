20f3eeb34e4a6e9320395d10bab4ece722dd885f
Asit Sengar <asitsengar28@gmail.com>
2026-06-07 14:47:47 +0530

    modifies pandas dataframe

--- pandas/pandas_dataframe.py
+++ pandas/pandas_dataframe.py
@@ -6,5 +6,13 @@ df = pd.DataFrame(data, index=["employe1", "employe2", "employe3"])
 print("the data frame is: ", df)
 # to print something at specific location
 print(df.loc["employe2"])
+# for adding a new coloumn
 df["job"] = ["software engineer", "housewife", "housewife"]
+# for adding a new row
+new_row = pd.DataFrame([])
 print(df)
+
+# now i want to import some files
+file = pd.read_csv(
+    r"C:\rough_folder\Machine_Learning_foundations\datasets\subs.csv")
+print(file)
